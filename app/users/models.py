873c2a28f86117a6f1fc55221194c4292bb6c516
Simowwn <125945116+Simowwn@users.noreply.github.com>
2025-02-27 17:24:02 +0800

    working login

--- app/users/models.py
+++ app/users/models.py
@@ -1,15 +1,19 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # ✅ UUID primary key
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     email = models.EmailField(unique=True)
     token = models.CharField(max_length=255, blank=True, null=True)
+    
     username = None
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]  # ✅ Fix field names
+    REQUIRED_FIELDS = ["first_name", "last_name"]  
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
